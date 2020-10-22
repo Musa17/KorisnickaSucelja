6b512e6ddd5e3a207fbd41c2d205a5ae124490a6
Musa17 <amusul00@fesb.hr>
2020-10-22 12:08:01 +0200

    adding Combobox

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,9 +5,9 @@ import Combobox from '../Combobox'
 import styles from './style.module.css'
 
 const Navigation = ({ activeTab }) => (
-  <section>
+  <section className={styles.navigation}>
       <NavigationBar activeTab={activeTab} />
-      <Combobox />
+      <Combobox/>
   </section>
 )
 
