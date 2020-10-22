543e7a137da0cdee62d377273cce7ab3fb8275d2
Musa17 <amusul00@fesb.hr>
2020-10-22 10:32:53 +0200

    Remove Navigation

--- src/components/NavigationHeader/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import styles from './style.module.css'
-
-const NavigationHeader = () => (
-  <section className={styles.navigationHeader}>
-  </section>
-)
-export default NavigationHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
 
 import ContactBar from '../components/contactBar'
-import NavigationHeader from '../components/NavigationHeader'
 
 const IndexPage = () => (
   <main>
    <ContactBar />
-   <NavigationHeader />
   </main>
 )
 
